5160cfb99296347dceb486f7b253ad7711c6ce89
Austin Rocha <austin.c.rocha@gmail.com>
2017-04-12 20:54:31 -0400

    materialize

--- appname/assets.py
+++ appname/assets.py
@@ -1,20 +1,13 @@
 from flask_assets import Bundle
-
 common_css = Bundle(
-    'css/vendor/bootstrap.min.css',
-    'css/vendor/helper.css',
     'css/main.css',
-    #'css/style.css',
-    #'css/materialize.min.css',
+    'css/materialize.min.css',
     filters='cssmin',
     output='public/css/common.css'
 )
-
 common_js = Bundle(
-    #'js/vendor/jquery.min.js',
-    'js/vendor/bootstrap.min.js',
     'js/jquery-3.2.1.min.js',
-    #'js/materialize.min.js',
+    'js/materialize.min.js',
     Bundle(
         'js/main.js',
         filters='jsmin'
